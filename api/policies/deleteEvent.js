3dac8c7f06834acea9c76fe7e6acaee5672bfea2
Brandt Heisey <heiseybj@state.gov>
2014-09-18 07:54:44 -0400

    eod commit

--- api/policies/deleteEvent.js
+++ api/policies/deleteEvent.js
@@ -11,29 +11,33 @@ var projUtil  = require('../services/utils/project');
 var eventUtil = require('../services/utils/event');
 
 
-module.exports = function canDeleteEvent (req, res, next) {
+module.exports = function deleteEvent (req, res, next) {
 
 	if (req.route.params.id) {
 	    var eventId = req.route.params.id;
 	    var userId = null;
-	    
-	    if (req.user) {
-	      userId = req.user[0].id;
+	    data = {};
 
-	      if ( req.user && req.user[0].isAdmin ){
-			//is admin ?
-			next();
-		  } else if ( projUtil.authorized(projectId,userId,projectCb) ) {
-			//is project owner ?
-			next();
-		  } else if ( event.isCreator(objectId,userId) ){
-			//is event creator ?
-			next();
-		  }
-		}
-	 	
-	 	//if we fall through to here something is amiss, so throw 403
- 		return res.send(403, { message: 'Not authorized.'});
+		Event.findOneById(eventId,function(err, event){ 
+			if (req.user) {
+		      userId = req.user[0].id;
+		      if ( req.user && req.user[0].isAdmin ){
+				//is admin ?
+				next();
+			  } else if ( projUtil.authorized(event.projectId,userId,projectCb) ) {
+				//is project owner ?
+				next();
+			  } else if ( eventUtil.isCreator(eventId,userId) ) {
+				//is event creator ?
+				next();
+			  } else {
+			  	return res.send(403, { message: 'Not authorized.'});
+			  }
+			}
+		});
+	  
+	} else {
+		//if we fall through to here something is amiss, so throw 403
+		return res.send(403, { message: 'Not authorized.'});
 	}
-
 }
--- config/policies.js
+++ config/policies.js
@@ -110,7 +110,7 @@ module.exports.policies = {
     'cancel': ['authenticated', 'requireUserId', 'addUserId', 'requireId'],
     'rsvp': ['authenticated', 'requireUserId', 'addUserId'],
     'ical': ['authenticated', 'addUserId', 'project'],
-    'destroy': ['authenticated', 'requireUserId', 'eventCreator','admin']
+    'destroy': ['authenticated', 'requireUserId', 'deleteEvent']
   },
 
   TagController : {
