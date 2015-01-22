60fc8b7ae696bbc2e730705c3096496c7df2748b
Dave Cole <david.cole@gsa.gov>
2015-01-22 00:49:16 -0500

    Set session maxAge to 30 days

--- config/session.js
+++ config/session.js
@@ -1,7 +1,7 @@
 /**
  * Session
- * 
- * Sails session integration leans heavily on the great work already done by Express, but also unifies 
+ *
+ * Sails session integration leans heavily on the great work already done by Express, but also unifies
  * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
  * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
  * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
@@ -9,19 +9,18 @@
  * For more information on configuring the session, check out:
  * http://sailsjs.org/#documentation
  */
-
 module.exports.session = {
 
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
-  secret: '0fa32505a53e70cd2b5626d70dd15b6c'
+  // forcing them to log in again.
+  secret: '0fa32505a53e70cd2b5626d70dd15b6c',
 
   // Set the cookie maximum age (timeout).  If this is not set, then cookies
   // will persist forever.
-  // cookie: {
-  //   maxAge: 60 * 60 * 1000  // example: 60 mins in miliseconds
-  // }
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+  }
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
