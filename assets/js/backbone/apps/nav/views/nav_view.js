4b563c68d27d08506cde3470c41917173eac1442
Corum <BMCorum@opm.gov>
2018-01-18 15:16:39 -0500

    Session expiration warning.
    Added an idle modal that alert the user that due to inactivity they will
    be logged out. The warning give the user a three minute window in which
    to reset his/her session. Sessions are currently set for 30 minutes,
    therefore the user will be alerted after 27 minutes of inactivity.
    
    Reset session timer on page navigation. Reset session timer on 30
    keypresses or one minute lapse in time since last keypress.

--- assets/js/backbone/apps/nav/views/nav_view.js
+++ assets/js/backbone/apps/nav/views/nav_view.js
@@ -11,6 +11,7 @@ var UIConfig = require('../../../config/ui.json');
 var Login = require('../../../config/login.json');
 var LoginController = require('../../login/controllers/login_controller');
 var NavTemplate = require('../templates/nav_template.html');
+var IdleModal = require('../../../components/modal_idle');
 var User = require('../../../../utils/user');
 
 var NavView = Backbone.View.extend({
@@ -27,15 +28,23 @@ var NavView = Backbone.View.extend({
 
     this.listenTo(window.cache.userEvents, 'user:login:success', function (userData) {
       self.doRender({ user: userData });
+      this.idleModal = new IdleModal({ el: '#login-wrapper' }).render();
+      this.idleModal.resetTimeout();
     });
 
     this.listenTo(window.cache.userEvents, 'user:login:close', function () {
       self.doingLogin = false;
     });
 
+    this.listenTo(window.cache.userEvents, 'user:request:logout', function () {
+      if(this.idleModal) this.idleModal.cleanup();
+      self.logout({});
+    });
+
     this.listenTo(window.cache.userEvents, 'user:logout', function () {
       self.doRender({ user: null });
       Backbone.history.navigate('', {trigger: true});
+      this.idleModal.cleanup();
       window.cache.userEvents.trigger('user:logout:success');
     });
 
@@ -70,6 +79,10 @@ var NavView = Backbone.View.extend({
   render: function () {
     var self = this;
     this.doRender({ user: window.cache.currentUser, systemName: window.cache.system.name });
+    if(window.cache.currentUser) {
+      this.idleModal = new IdleModal({ el: '#login-wrapper' }).render();
+      this.idleModal.resetTimeout();
+    }
     return this;
   },
 
@@ -115,6 +128,9 @@ var NavView = Backbone.View.extend({
   cleanup: function () {
     if (this.loginController) {
       this.loginController.cleanup();
+    }
+    if (this.idleModal) {
+      this.idleModal.cleanup();
     }
     removeView(this);
   },
