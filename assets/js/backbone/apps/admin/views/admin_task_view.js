8e04575aa9a52cfbbec23c866bc37e11c7c94d34
Brice Corum <bmcorum@gmail.com>
2018-06-18 15:58:13 -0400

    Admin - Update confirmation modal to meet design system standards
    - Update delete confirmation to use design system modal
    
    #1843

--- assets/js/backbone/apps/admin/views/admin_task_view.js
+++ assets/js/backbone/apps/admin/views/admin_task_view.js
@@ -18,10 +18,18 @@ var AdminTaskView = Backbone.View.extend({
     this.data = {
       page: 1,
     };
+    this.baseModal = {
+      el: '#site-modal',
+      secondary: {
+        text: 'Cancel',
+        action: function () {
+          this.modal.cleanup();
+        }.bind(this),
+      },
+    };
   },
 
   render: function () {
-    var view = this;
     var url = '/admin/tasks';
     if (this.options.agencyId) url = url + '/' + this.options.agencyId;
     Backbone.history.navigate(url);
@@ -31,13 +39,13 @@ var AdminTaskView = Backbone.View.extend({
       data: this.data,
       dataType: 'json',
       success: function (data) {
-        view.tasks = data;
+        this.tasks = data;
         var template = _.template(AdminTaskTemplate)(data);
         $('#search-results-loading').hide();
-        view.$el.html(template);
-        view.$el.show();
-        view.renderTasks(view.tasks);
-      },
+        this.$el.html(template);
+        this.$el.show();
+        this.renderTasks(view.tasks);
+      }.bind(this),
     });
     return this;
   },
@@ -55,42 +63,38 @@ var AdminTaskView = Backbone.View.extend({
     this.renderTasks(this.tasks);
   },
 
+  collectEventData: function (event) {
+    event.preventDefault();
+    if (this.modalComponent) this.modalComponent.cleanup();
+    return { 
+      id: $(event.currentTarget).data('task-id'),
+      title: $( event.currentTarget ).data('task-title'),
+    };
+  },
+
   /*
    * Open a "submitted" task from the admin task view.
    * @param { jQuery Event } event
    */
   openTask: function (event) {
-    event.preventDefault();
-    if (this.modalComponent) this.modalComponent.cleanup();
-
-    var view = this;
-    var id = $(event.currentTarget).data('task-id');
-    var title = $( event.currentTarget ).data('task-title');
-
+    var data = this.collectEventData(event);
     $('body').addClass('modal-is-open');
 
-    this.modal = new Modal({
-      el: '#site-modal',
+    this.modal = new Modal(_.extend(this.baseModal, {
       id: 'confirm-publish',
       modalTitle: 'Confirm publish',
       alert: {
         type: 'error',
         text: 'Error publishing task.',
       },
-      modalBody: 'Are you sure you want to publish <strong>' + title + '</strong>?',
+      modalBody: 'Are you sure you want to publish <strong>' + data.title + '</strong>?',
       primary: {
         text: 'Publish',
         action: function () {
-          this.submitPublish.bind(this)(id);
+          this.submitPublish.bind(this)(data.id);
         }.bind(this),
       },
-      secondary: {
-        text: 'Cancel',
-        action: function () {
-          this.modal.cleanup();
-        }.bind(this),
-      },
-    }).render();
+    })).render();
   },
 
   submitPublish: function (id) {
@@ -99,32 +103,57 @@ var AdminTaskView = Backbone.View.extend({
       data: {'id': id, 'state': 'open'},
       type: 'PUT',
     }).done(function ( model, response, options ) {
-      $('.usajobs-modal__canvas-blackout').remove();
-      $('.modal-is-open').removeClass();
-      this.render();
-      this.modal.cleanup();
+      this.cleanupModal();
     }.bind(this)).fail(function (error) {
-      $('#confirm-publish').addClass('usajobs-modal--error');
-      $('.usajobs-modal__body').html('There was an error attempting to publish this opportunity.');
-      $('#usajobs-modal-heading').hide();
-      $('#alert-modal__heading').show();
-      $('#primary-btn').hide();
+      this.displayError('confirm-publish', 'There was an error attempting to publish this opportunity.');
     }.bind(this));
   },
 
-  deleteTask: function (e) {
-    var view = this,
-        id = $(e.currentTarget).data('task-id'),
-        title = $(e.currentTarget).data('task-title');
-    e.preventDefault();
-    if (window.confirm('Are you sure you want to delete "' + title + '"?')) {
-      $.ajax({
-        url: '/api/task/' + id,
-        type: 'DELETE',
-      }).done(function () {
-        view.render();
-      });
-    }
+  deleteTask: function (event) {
+    var data = this.collectEventData(event);
+    $('body').addClass('modal-is-open');
+
+    this.modal = new Modal(_.extend(this.baseModal, {
+      id: 'confirm-deletion',
+      modalTitle: 'Confirm deletion',
+      alert: {
+        type: 'error',
+        text: 'Error deleting task.',
+      },
+      modalBody: 'Are you sure you want to delete <strong>' + data.title + '</strong>? <strong>This cannot be undone</strong>.',
+      primary: {
+        text: 'Delete',
+        action: function () {
+          this.submitDelete.bind(this)(data.id);
+        }.bind(this),
+      },
+    })).render();
+  },
+
+  submitDelete: function (id) {
+    $.ajax({
+      url: '/api/task/' + id,
+      type: 'DELETE',
+    }).done(function ( model, response, options ) {
+      this.cleanupModal();
+    }.bind(this)).fail(function (error) {
+      this.displayError('confirm-deletion', 'There was an error attempting to delete this opportunity.');
+    }.bind(this));
+  },
+
+  cleanupModal: function () {
+    $('.usajobs-modal__canvas-blackout').remove();
+    $('.modal-is-open').removeClass();
+    this.render();
+    this.modal.cleanup();
+  },
+
+  displayError: function (modalId, error) {
+    $('#' + modalId).addClass('usajobs-modal--error');
+    $('.usajobs-modal__body').html(error);
+    $('#usajobs-modal-heading').hide();
+    $('#alert-modal__heading').show();
+    $('#primary-btn').hide();
   },
 
   cleanup: function () {
