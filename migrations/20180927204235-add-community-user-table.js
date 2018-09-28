7a4d27b01218a8ea2d1e8763695d7bae71992047
Jodi <jodi.hanson@outlook.com>
2018-09-28 09:38:14 -0400

    Create Community Table
    Created scripts community, community_skill, community_user tables for Community.
    
    #2550

--- migrations/20180927204235-add-community-user-table.js
+++ migrations/20180927204235-add-community-user-table.js
@@ -15,17 +15,17 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = function (db, callback) {
-  db.createTable('application_task', {
-    application_task_id: { type: 'bigint', primaryKey: true, autoIncrement: true },
-    application_id: { type: 'bigint', notNull: true },
-    user_id: { type: 'bigint', notNull: true },
-    task_id: { type: 'bigint', notNull: true },
-    sort_order: { type: 'int', notNull: true },
+  db.createTable('community_user', {
+    community_user_id: { type: 'bigint', primaryKey: true, autoIncrement: true },
+    community_id: { type: 'bigint', notNull: true },
+    user_id: { type: 'character varying', notNull: true, defaultValue: '' },
+    is_manager: { type: 'boolean' },
     created_at: { type: 'timestamp with time zone' },
     updated_at: { type: 'timestamp with time zone' },
   }, callback);
 };
 
 exports.down = function (db, callback) {
-  db.dropTable('application_task', callback);
+  db.dropTable('community_user', callback);
 };
+
