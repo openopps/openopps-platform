8907f3dcb643fc49ebe42017f874bfa9fabaf4ba
Abushanab <Saleim.Abushanab@opm.gov>
2019-02-06 14:17:52 -0500

    added term from url to keyword field and corrected location for internship page

--- assets/js/backbone/apps/tasks/search/views/internship_search_view.js
+++ assets/js/backbone/apps/tasks/search/views/internship_search_view.js
@@ -48,7 +48,6 @@ var InternshipListView = Backbone.View.extend({
     this.initializeBureaus();
     this.taskFilteredCount = 0;
     this.appliedFilterCount = getAppliedFiltersCount(this.filters, this.agency);
-   
   },
     
   render: function () {
@@ -58,7 +57,7 @@ var InternshipListView = Backbone.View.extend({
       user: window.cache.currentUser,
       ui: UIConfig,
       agencyName: this.userAgency.name,
-      term: this.filters.term,
+      term: this.filters.term ? this.filters.term : "",
       filters: this.filters,
       taskFilteredCount: this.taskFilteredCount,
       appliedFilterCount: this.appliedFilterCount,    
