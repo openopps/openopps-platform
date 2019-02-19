e9ecfc44500c961c7fcc2253e1ac542ba10c462f
Ray Williams <ray.williams@gmail.com>
2019-02-19 11:28:03 -0500

    Department of State: Right Rail of Submission Confirmation Page
    - Created generic javascript for drawers where only one can be open
    - Created generic javascript for accordion where only one can be open
    
    #2994

--- assets/js/backbone/apps/apply/views/apply_congratulations_view.js
+++ assets/js/backbone/apps/apply/views/apply_congratulations_view.js
@@ -5,12 +5,8 @@ var $ = require('jquery');
 var ApplyCongratulationsView = Backbone.View.extend({
 
   events: {
-    'click .a1' : 'toggleAccordion1',
-    'click .a2' : 'toggleAccordion2',
-    'click .d1' : 'toggleDrawer1',
-    'click .d2' : 'toggleDrawer2',
-    'click .d3' : 'toggleDrawer3',
-    'click .d4' : 'toggleDrawer4',
+    'click .accordion-1'            : 'toggleAccordion',
+    'click .usajobs-drawer-button'  : 'toggleDrawer',
   },
 
   // initialize components and global functions
@@ -24,148 +20,50 @@ var ApplyCongratulationsView = Backbone.View.extend({
     $('#search-results-loading').hide();
     this.$el.localize();
 
-    this.data = {
-      a1: {
-        open: false,
-      },
-      a2: {
-        open: false,
-      },
-      d1: {
-        open: false,
-      },
-      d2: {
-        open: false,
-      },
-      d3: {
-        open: false,
-      },
-      d4: {
-        open: false,
-      },
-    };
-
     $('.apply-hide').hide();
 
     return this;
   },
   
-  toggleAccordion1: function (e) {
+  toggleAccordion: function (e) {
     var element = $(e.currentTarget);
-    this.data.a1.open = !this.data.a1.open;
-    element.attr('aria-expanded', this.data.a1.open);
-    $('#a1').attr('aria-hidden', !this.data.a1.open);
-
-    $('.a2').attr('aria-expanded', false);
-    $('#a2').attr('aria-hidden', true);
-    this.data.a2.open = false;
+    var target = element.siblings('.usa-accordion-content');
+    var otherElements = element.parent('li').siblings('li').find('.usa-accordion-button');
+    var otherTargets = element.parent('li').siblings('li').find('.usa-accordion-content');
+    var open = element.attr('aria-expanded') == 'true';
+    if (!open) {
+      otherElements.attr('aria-expanded', false);
+      otherTargets.attr('aria-hidden', true);
+      element.attr('aria-expanded', true);
+      target.attr('aria-hidden', false);
+    } else {
+      element.attr('aria-expanded', false);
+      target.attr('aria-hidden', true);
+    }
   },
-  
-  toggleAccordion2: function (e) {
+
+  toggleDrawer: function (e) {
     var element = $(e.currentTarget);
-    this.data.a2.open = !this.data.a2.open;
-    element.attr('aria-expanded', this.data.a2.open);
-    $('#a2').attr('aria-hidden', !this.data.a2.open);
-
-    $('.a1').attr('aria-expanded', false);
-    $('#a1').attr('aria-hidden', true);
-    this.data.a1.open = false;
-  },
-
-  toggleDrawer1: function (e) {
-    if (!this.data.d1.open) {
-      $('#find').slideDown('fast', function () {
-        $('.d2, .d3, .d4').attr('aria-expanded', false);
-        $('#resume-search, #save-search, #learn-more-govt').attr('aria-hidden', true);
-        $('#resume-search, #save-search, #learn-more-govt').css('display', '');
+    var target = element.siblings('.usajobs-drawer-content');
+    var otherElements = element.parent('.usajobs-drawer').siblings().find('.usajobs-drawer-button');
+    var otherTargets = element.parent('.usajobs-drawer').siblings().find('.usajobs-drawer-content');
+    var open = element.attr('aria-expanded') == 'true';
+    if (!open) {
+      otherElements.attr('aria-expanded', false);
+      otherTargets.attr('aria-hidden', true).css('display', '');
+      target.slideDown('fast', function () {
         $('html, body').animate({
-          scrollTop: $('.d1').offset().top,
+          scrollTop: element.offset().top,
         });
-        $('.d1').attr('aria-expanded', 'true');
-        $('#find').attr('aria-hidden', 'false');
+        element.attr('aria-expanded', 'true');
+        target.attr('aria-hidden', 'false');
       });
     } else {
-      $('#find').slideUp(function () {
-        $('.d1').attr('aria-expanded', 'false');
-        $('#find').attr('aria-hidden', 'true');
+      target.slideUp(function () {
+        element.attr('aria-expanded', 'false');
+        target.attr('aria-hidden', 'true');
       });
     }
-    this.data.d1.open = !this.data.d1.open;
-    this.data.d2.open = false;
-    this.data.d3.open = false;
-    this.data.d4.open = false;
-  },
-
-  toggleDrawer2: function (e) {
-    if (!this.data.d2.open) {
-      $('#resume-search').slideDown('fast', function () {
-        $('.d1, .d3, .d4').attr('aria-expanded', false);
-        $('#find, #save-search, #learn-more-govt').attr('aria-hidden', true);
-        $('#find, #save-search, #learn-more-govt').css('display', '');
-        $('html, body').animate({
-          scrollTop: $('.d2').offset().top,
-        });
-        $('.d2').attr('aria-expanded', 'true');
-        $('#resume-search').attr('aria-hidden', 'false');
-      });
-    } else {
-      $('#resume-search').slideUp(function () {
-        $('.d2').attr('aria-expanded', 'false');
-        $('#resume-search').attr('aria-hidden', 'true');
-      });
-    }
-    this.data.d1.open = false;
-    this.data.d2.open = !this.data.d2.open;
-    this.data.d3.open = false;
-    this.data.d4.open = false;
-  },
-
-  toggleDrawer3: function (e) {
-    if (!this.data.d3.open) {
-      $('#save-search').slideDown('fast', function () {
-        $('.d1, .d2, .d4').attr('aria-expanded', false);
-        $('#find, #resume-search, #learn-more-govt').attr('aria-hidden', true);
-        $('#find, #resume-search, #learn-more-govt').css('display', '');
-        $('html, body').animate({
-          scrollTop: $('.d3').offset().top,
-        });
-        $('.d3').attr('aria-expanded', 'true');
-        $('#save-search').attr('aria-hidden', 'false');
-      });
-    } else {
-      $('#save-search').slideUp(function () {
-        $('.d3').attr('aria-expanded', 'false');
-        $('#save-search').attr('aria-hidden', 'true');
-      });
-    }
-    this.data.d1.open = false;
-    this.data.d2.open = false;
-    this.data.d3.open = !this.data.d3.open;
-    this.data.d4.open = false;
-  },
-
-  toggleDrawer4: function (e) {
-    if (!this.data.d4.open) {
-      $('#learn-more-govt').slideDown('fast', function () {
-        $('.d1, .d2, .d3').attr('aria-expanded', false);
-        $('#find, #resume-search, #save-search').attr('aria-hidden', true);
-        $('#find, #resume-search, #save-search').css('display', '');
-        $('html, body').animate({
-          scrollTop: $('.d4').offset().top,
-        });
-        $('.d4').attr('aria-expanded', 'true');
-        $('#learn-more-govt').attr('aria-hidden', 'false');
-      });
-    } else {
-      $('#learn-more-govt').slideUp(function () {
-        $('.d4').attr('aria-expanded', 'false');
-        $('#learn-more-govt').attr('aria-hidden', 'true');
-      });
-    }
-    this.data.d1.open = false;
-    this.data.d2.open = false;
-    this.data.d3.open = false;
-    this.data.d4.open = !this.data.d4.open;
   },
 
   cleanup: function () {
