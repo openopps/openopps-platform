ea5eac174cacafd73b6cb213b2f6cbb45f5f202d
Brice Corum <bmcorum@gmail.com>
2019-02-22 16:21:54 -0500

    Student internship application
    - Refactor code for program selection and deletions

--- assets/js/backbone/apps/apply/views/apply_view.js
+++ assets/js/backbone/apps/apply/views/apply_view.js
@@ -8,6 +8,7 @@ const templates = require('./templates');
 const nextSteps = require('./next_steps');
 
 //utility functions
+var Program = require('./program');
 var Experience = require('./experience');
 var Language = require('./language');
 var Education = require('./education');
@@ -21,7 +22,9 @@ var ApplyView = Backbone.View.extend({
     'click .apply-continue'                                       : 'applyContinue',
     'click .usajobs-drawer-button'                                : 'toggleDrawers',
     'click #back'                                                 : 'backClicked',
-    'click .program-delete'                                       : 'deleteProgram',
+
+    // program events
+    'click .program-delete'                                       : function (e) { this.callMethod(Program.deleteProgram, e); },
 
     //experience events
     'change [name=has_overseas_experience]'                       : function () { this.callMethod(Experience.toggleOverseasExperienceDetails); },
@@ -124,20 +127,6 @@ var ApplyView = Backbone.View.extend({
 
   callMethod: function (method, e) {
     method.bind(this)(e);
-  },
-
-  deleteProgram: function (e) {
-    e.preventDefault && e.preventDefault();
-    $.ajax({
-      url: '/api/application/' + this.data.applicationId + '/task/' + e.currentTarget.dataset.taskId,
-      method: 'DELETE',
-    }).done(function () {
-      this.data.tasks.splice(e.currentTarget.dataset.index, 1);
-      this.data[['firstChoice', 'secondChoice', 'thirdChoice'][e.currentTarget.dataset.index]] = null;
-      this.$el.html(templates.getTemplateForStep(1)(this.data));
-    }.bind(this)).fail(function () {
-      showWhoopsPage();
-    });
   },
 
   toggleDrawers: function (e) {
