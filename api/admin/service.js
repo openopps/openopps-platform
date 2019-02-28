4c0caebd5be5110b7cc784e93dc1ba7f8dfff1a2
Brice Corum <bmcorum@gmail.com>
2019-02-28 09:35:49 -0500

    Community Admins
    - Fix issue where community administrators cound not approve
    or delete opportunities
    
    #3034

--- api/admin/service.js
+++ api/admin/service.js
@@ -402,6 +402,19 @@ async function canChangeOwner (user, taskId) {
   return task && (task.restrict.name == agency.name);
 }
 
+async function canCommunityChangeOwner (user, taskId) {
+  var task = await dao.Task.findOne('id = ?', taskId).catch((err) => { 
+    return undefined;
+  });
+  if (task && task.communityId) {
+    return (await dao.CommunityUser.findOne('user_id = ? and community_id = ?', user.id, task.communityId).catch(() => {
+      return {};
+    })).isManager;
+  } else {
+    return false;
+  }
+}
+
 async function getOwnerOptions (taskId, done) {
   var task = await dao.Task.findOne('id = ?', taskId).catch((err) => { 
     return undefined;
@@ -416,8 +429,8 @@ async function getCommunityOwnerOptions (taskId, done) {
   var task = await dao.Task.findOne('id = ?', taskId).catch((err) => { 
     return undefined;
   });
-  if (task) {
-    done(await dao.User.query(dao.query.ownerCommunityListQuery));  
+  if (task && task.communityId) {
+    done(await dao.User.query(dao.query.ownerCommunityListQuery, task.communityId));  
   } else {
     done(undefined, 'Unable to locate specified task');
   }
@@ -532,6 +545,7 @@ module.exports = {
   getOwnerOptions: getOwnerOptions,
   getCommunityOwnerOptions:getCommunityOwnerOptions,
   changeOwner: changeOwner,
+  canCommunityChangeOwner: canCommunityChangeOwner,
   assignParticipant: assignParticipant,
   createAuditLog: createAuditLog,
   getCommunityTaskStateMetrics:getCommunityTaskStateMetrics,
