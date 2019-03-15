e6afea7060a04efe97d99f50ce606e709dc479d5
Kaye <Wayne.Kaye@opm.gov>
2019-03-15 09:28:29 -0400

    36116 - Agency Portal - Applicant Review - Applicant Preview - Data - Transcripts

---
+++ utils/tamper-proof.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+module.exports = {
+  Hmac: function (p1) {
+    var hash = crypto.createHmac('sha256', Buffer.from(openopps.auth.documentSecret, 'utf8')).update(p1).digest('base64');
+    return hash.replace('=', '').replace('+', '-').replace('/', '_');
+  },
+};
