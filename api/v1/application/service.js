43fb093226fad00bb1ae6a05bbc2c9685f9a1e51
Marshall Fordham <marshallfordham@hotmail.com>
2019-03-21 10:26:01 -0400

    Fix panel issues from merge.  TFS 36500

--- api/v1/application/service.js
+++ api/v1/application/service.js
@@ -4,8 +4,8 @@ const tamperProof = require('../../../utils/tamper-proof');
 
 var service = {};
 
-service.getApplicationSummary = async function (applicationId, sub, auth_time) {
-  var results = (await dao.TaskListApplication.db.query(dao.query.ApplicantSummary, applicationId)).rows[0];
+service.getApplicationSummary = async function (taskListApplicationId, sub, auth_time) {
+  var results = (await dao.TaskListApplication.db.query(dao.query.ApplicantSummary, taskListApplicationId)).rows[0];
   if (results.transcript_id !== null) {
     results.transcript_key = tamperProof.Hmac([sub, auth_time, results.transcript_id].join('|'));
   }
