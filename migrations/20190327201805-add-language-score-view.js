7893468f025e8df6c74578c1d51c1defd7f95fc7
Marshall Fordham <marshallfordham@hotmail.com>
2019-04-01 13:18:31 -0400

    Update migrate scripts

--- migrations/20190327201805-add-language-score-view.js
+++ migrations/20190327201805-add-language-score-view.js
@@ -16,31 +16,48 @@ exports.setup = function(options, seedLink) {
 
 exports.up = function (db, callback) {
   db.runSql(`
-    create view applicant_language_score as
-      select
-        task.id as task_id,
-        app.application_id,
-        als.language_id,
-        readScore.score as reading_score,
-        writeScore.score as writing_score,
-        speakScore.score as speaking_score,
-        (readScore.score + writeScore.score + speakScore.score) as language_score,	
-        ls.weight_factor as language_weight_factor,
-        (readScore.score + writeScore.score + speakScore.score) * ls.weight_factor as weighted_language_score
-      from
-        application_language_skill als
-        inner join language_proficiency_score readScore on als.reading_proficiency_id = readScore.proficiency_id
-        inner join language_proficiency_score writeScore on als.writing_proficiency_id = writeScore.proficiency_id
-        inner join language_proficiency_score speakScore on als.speaking_proficiency_id = speakScore.proficiency_id
-        inner join application app on als.application_id = app.application_id
-        inner join application_task appt on app.application_id = appt.application_id
-        inner join task on appt.task_id = task.id
-        inner join language_skill ls on task.id = ls.task_id and als.language_id = ls.language_id
+    create or replace function get_applicant_language_score (p_cycle_id int8) 
+    returns table (
+      task_id int4,
+      application_id int8,
+      language_id int4,
+      reading_score numeric,
+      writing_score numeric,
+      speaking_score numeric,
+      language_score numeric,
+      language_weight_factor float4,
+      weighted_language_score float8
+    ) 
+    as $$
+    begin
+    return QUERY select
+      task.id as task_id,
+      app.application_id,
+      als.language_id,
+      readScore.score as reading_score,
+      writeScore.score as writing_score,
+      speakScore.score as speaking_score,
+      (readScore.score + writeScore.score + speakScore.score) as language_score,    
+      ls.weight_factor as language_weight_factor,
+      (readScore.score + writeScore.score + speakScore.score) * ls.weight_factor as weighted_language_score
+    from
+      application_language_skill als
+      inner join language_proficiency_score readScore on als.reading_proficiency_id = readScore.proficiency_id
+      inner join language_proficiency_score writeScore on als.writing_proficiency_id = writeScore.proficiency_id
+      inner join language_proficiency_score speakScore on als.speaking_proficiency_id = speakScore.proficiency_id
+      inner join application app on als.application_id = app.application_id
+      inner join task on app.cycle_id = task.cycle_id and app.community_id = task.community_id
+      inner join language_skill ls on task.id = ls.task_id and als.language_id = ls.language_id
+    where
+      task.cycle_id = p_cycle_id;
+    end; $$ 
+    
+    language 'plpgsql';
   `, callback );
 }
 
 exports.down = function (db, callback) {
-  db.runSql('drop view applicant_language_score', callback);
+  db.runSql('drop function get_applicant_language_score(bigint)', callback);
 }
 
 exports._meta = {
--- migrations/20190328201640-add-applicant-score-view.js
+++ migrations/20190328201640-add-applicant-score-view.js
@@ -10,32 +10,50 @@ var seed;
   */
  exports.up = function (db, callback) {
   db.runSql(`
-    create view applicant_score as
-      select
-        t.id as task_id,
-        app.application_id,
-        app.cumulative_gpa,
-        app.random_factor,
-        t.random_weight,
-        app.random_factor * t.random_weight as weighted_random_factor,
-        case
-          when gpa_range.max_gpa - gpa_range.min_gpa = 0 then 0
-          else ((app.cumulative_gpa - gpa_range.min_gpa) / (gpa_range.max_gpa - gpa_range.min_gpa)) * 10.0
-        end as weighted_gpa
-      from
-        application app
-        inner join application_task appt on app.application_id = appt.application_id
-        inner join task t on appt.task_id = t.id
-        inner join (
-          select max(cumulative_gpa) max_gpa, min(cumulative_gpa) min_gpa, community_id, cycle_id
-          from application
-          group by community_id, cycle_id
-        ) gpa_range on app.community_id = gpa_range.community_id and app.cycle_id = gpa_range.cycle_id
+      create or replace function get_applicant_score (p_cycle_id int8) 
+      returns table (
+      task_id int4,
+      application_id int8,
+      preference int4,
+      cumulative_gpa numeric,
+      random_factor numeric,
+      random_weight float4,
+      weighted_random_factor float8,
+      weighted_gpa numeric
+      ) 
+      as $$
+      begin
+      return QUERY select
+            t.id as task_id,
+            app.application_id,
+            appt.sort_order as preference,
+            app.cumulative_gpa,
+            app.random_factor,
+            t.random_weight,
+            app.random_factor * t.random_weight as weighted_random_factor,
+            case
+              when gpa_range.max_gpa - gpa_range.min_gpa = 0 then 0
+              else ((app.cumulative_gpa - gpa_range.min_gpa) / (gpa_range.max_gpa - gpa_range.min_gpa)) * 10.0
+            end as weighted_gpa
+          from
+            application app
+            inner join task t on app.cycle_id = t.cycle_id and app.community_id = t.community_id
+            left outer join application_task appt on app.application_id = appt.application_id and appt.task_id = t.id
+            inner join (
+              select max(application.cumulative_gpa) max_gpa, min(application.cumulative_gpa) min_gpa, community_id, cycle_id
+              from application
+              group by community_id, cycle_id
+            ) gpa_range on app.community_id = gpa_range.community_id and app.cycle_id = gpa_range.cycle_id
+      where
+        t.cycle_id = p_cycle_id;
+      end; $$ 
+      
+      language 'plpgsql';
   `, callback );
 }
 
 exports.down = function (db, callback) {
-  db.runSql('drop view applicant_score', callback);
+  db.runSql('drop function get_applicant_score(bigint)', callback);
 }
 
 exports._meta = {
