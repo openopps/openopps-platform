ed709e2d4623873608707a89854d2d2049a93259
Marshall Fordham <marshallfordham@hotmail.com>
2019-04-02 11:26:09 -0400

    Create project test data.  TFS 36810

--- api/v1/faker/dao.js
+++ api/v1/faker/dao.js
@@ -8,9 +8,16 @@ dao.query.countryData = `
     from country inner join country_subdivision on country.code = country_subdivision.parent_code
 `;
 
+dao.query.bureauData = `
+    select bureau.bureau_id, office.office_id
+    from bureau inner join office on bureau.bureau_id = office.bureau_id
+`;
+
 module.exports = function (db) {
     dao.Application = pgdao({ db: db, table: 'application' });
     dao.ApplicationTask = pgdao({ db: db, table: 'application_task' });
+    dao.Community = pgdao({ db: db, table: 'community' });
+    dao.Cycle = pgdao({ db: db, table: 'cycle' });
     dao.Education = pgdao({ db: db, table: 'education' });
     dao.Language = pgdao({ db: db, table: 'language' });
     dao.Experience = pgdao({ db: db, table: 'experience' });
@@ -21,8 +28,10 @@ module.exports = function (db) {
     dao.TaskListApplication = pgdao({ db: db, table: 'task_list_application' });
     dao.LookupCode = pgdao({ db: db, table: 'lookup_code' });
     dao.ApplicationLanguageSkill = pgdao({ db: db, table: 'application_language_skill' });
+    dao.LanguageSkill = pgdao({ db: db, table: 'language_skill' });
     dao.ApplicationSkill = pgdao({ db: db, table: 'application_skill' });
     dao.TagEntity = pgdao({ db: db, table: 'tagentity' });
+    dao.TaskTag = pgdao({ db: db, table: 'tagentity_tasks__task_tags' });
     return dao;
 };
   
