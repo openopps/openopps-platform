6fce80af2f76af93ac2589b0249d4bba056624e6
Williams <Ray.Williams@opm.gov>
2019-05-22 14:04:32 -0400

    Display full name in community dropdown
    - Wrap text in selects that have really long options
    
    #3344

--- assets/js/backbone/global-utils.js
+++ assets/js/backbone/global-utils.js
@@ -16,6 +16,78 @@ global.showWhoopsPage = function () {
   window.scrollTo(0, 0);
 };
 
+global.detectBrowser = function () {
+  var ua = navigator.userAgent, tem;
+  M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return 'IE ' + (tem[1] || '');
+  }
+  if(M[1] === 'Chrome'){
+    tem= ua.match(/\bOPR\/(\d+)/);
+    if (tem != null) return 'Opera ' + tem[1];
+  }
+  M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+  return M;        
+};
+
+global.setSelectHeight = function (ele) {
+  var newHeight = $(ele).next('div').outerHeight() - 20;
+  $(ele).height(newHeight);
+};
+
+/**
+ * Takes a select element and wraps the text
+ * 
+ * @param {HTMLElement} ele the select element
+ */
+global.initializeSelectWrapping = function (ele) {
+  var myBrowser = detectBrowser();
+  var renderWidth = $(ele).outerWidth();
+  var renderWidthFixed = renderWidth;
+  var borderwidth = $(ele).css('border-bottom-width');
+  var defaultValue = $(ele).find('option:selected').text().trim();
+  if (borderwidth == '0px') { borderwidth = '1px'; /*FF*/ }
+  $(ele).css({ cursor: 'pointer' });
+    
+  // set by browser (different buttons):
+  var topParsed = Math.round(parseInt(borderwidth.replace(/[^0-9.]+/g,'')));
+  switch(myBrowser[0]) {
+    case 'MSIE': renderWidthFixed = renderWidth - 28; break;
+    case 'I': renderWidthFixed = renderWidth - 28; break;                 
+    case 'Chrome': renderWidthFixed = renderWidth - 30; break;
+    case 'Firefox': 
+      renderWidthFixed = renderWidth - 27; 
+      borderRightParsed= '0'; 
+      if (index > 0) topParsed++;
+      break; 
+  }
+  //wrap + add a overlapping layer that will hide content and calculate the correct height:
+  $(ele).wrap($('<div />').css({width:renderWidth, margin:0, padding:0, position:'relative'}));
+  $(ele).after($('<div>' + defaultValue + '</div>')
+    .css({
+      minHeight: 45,
+      padding: '1rem 0.7em',
+      width: renderWidthFixed,
+      backgroundColor: 'white',
+      whiteSpace: 'pre-wrap',
+      position: 'absolute',
+      top: topParsed,
+      cursor: 'default',
+      left: borderwidth,
+    })
+  );
+  //set select box new height:
+  setSelectHeight(ele);
+    
+  //append change behavior:
+  $(ele).change(function () {
+    $(ele).next('div').text($(ele).find('option:selected').text().trim());
+    setSelectHeight(ele);
+  });
+};
+
 /**
  * Takes a name and pulls the first letter of first name
  * and first letter of last name if it exist. If name is 
