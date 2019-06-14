8826f7fc26135a44764a8dbdb11c8d2f72aaec53
Brice Corum <bmcorum@gmail.com>
2019-06-14 15:01:41 -0400

    Elasticsearch for people search
    - Establish user index for profiles
    - Index federal user on log in
    - Add ability to search against new elastic user index
    
    #3569

--- elastic/initializer.js
+++ elastic/initializer.js
@@ -25,7 +25,34 @@ initalizer.InitializeTaskTemplate = async function (client) {
     },
   }).catch((reason) => {
     console.error(reason);
-  }).then( ()=>{
+  }).then(() => {
     console.log('updated task_template template');
+  });
+};
+
+/**
+ * @param {elasticsearch.Client} client 
+ */
+initalizer.InitializeUserTemplate = async function (client) {
+  if(!client || typeof(client)==='undefined'){
+    console.warn('Elasic client not configured!');
+    return;
+  }
+  var settings_file = JSON.parse(fs.readFileSync('./elastic/user_settings.json'));
+  var mappings_file = JSON.parse(fs.readFileSync('./elastic/user_mapping.json'));
+  await client.indices.putTemplate({
+    name: 'user_template',
+    order: 1,
+    create: false,
+    body: {
+      template: 'user*',
+      index_patterns: ['user*'],
+      settings: settings_file,
+      mappings: mappings_file,
+    },
+  }).catch((reason) => {
+    console.error(reason);
+  }).then(() => {
+    console.log('updated user_template template');
   });
 };
