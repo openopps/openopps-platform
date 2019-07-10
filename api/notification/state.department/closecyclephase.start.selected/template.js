069b52ed1a2eace43dbd2b47b6981fbadffa0654
Bruno <24001003530623@FEDIDCARD.GOV>
2019-07-10 09:13:59 -0400

    37439: Applicant Review - Manual Trigger - Close Cycle - Applicant Exclusive Posting JOA Email Template

---
+++ api/notification/state.department/closecyclephase.start.selected/template.js
@@ -0,0 +1,16 @@
+module.exports = {
+  subject: 'U.S. Department of State Student Internship Program (Unpaid)—You have been selected for an internship.',
+  to: '<%= email %>',
+  data: function (model, done) {
+    var data = {
+      email: model.email,
+      given_name: model.given_name,         
+      office: model.office,
+      session: model.session,
+      jobLink: model.jobLink,
+      contact_email: model.jobLink,
+      title: model.title,
+    };
+    done(null, data);
+  },
+};
