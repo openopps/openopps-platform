fbc0e5869ac9186236d3cd32e1f1ddbc8956f817
tanichacko <40576337+tanichacko@users.noreply.github.com>
2019-08-23 10:18:18 -0400

    Admin Dashboard: Section 3: Completed Opportunities
    - Fix calculation for published,completed,canceled
    -clean up code
    
    #2369

--- api/admin/taskmetrics/taskDecorator.js
+++ api/admin/taskmetrics/taskDecorator.js
@@ -3,54 +3,33 @@ const DateCodeGenerator = require('./dateCodeGenerator');
 
 function TaskDecorator (task) {
   this.object = _.clone(task);
+
 }
 
 _.extend(TaskDecorator.prototype, {
-  isPublished: function () {
-    return _.includes(['open', 'assigned', 'completed'], this.object.state);
-  },
+
 
   isAssigned: function () {
     return _.includes(['assigned', 'completed'], this.object.state);
   },
 
-  isCompleted: function () {
-    return _.includes(['completed'], this.object.state);
-  },
-  isCanceled: function () {
-    return _.includes(['canceled'], this.object.state);
-  },
+  
 
   isNotArchived: function () {
     return this.object.state != 'archived';
   },
 
+
   decorate: function (group) {
-    this.normalizeDates();
-    _.each(['isPublished', 'isAssigned', 'isCompleted', 'isNotArchived','isCanceled'], function (attr) {
+ 
+    _.each(['isAssigned', 'isNotArchived'], function (attr) {
       this.object[attr] = this[attr]();
     }.bind(this));
     this.addDateCodes(group);
     return this.object;
   },
 
-  normalizeDates: function () {
-    var createdAt = this.object.createdAt;
-    if (!this.object.publishedAt && this.isPublished()) {
-      this.object.publishedAt = createdAt;
-    }
-
-    if (!this.object.assignedAt && this.isAssigned()) {
-      this.object.assignedAt = createdAt;
-    }
-
-    if (!this.object.completedAt && this.isCompleted()) {
-      this.object.completedAt = createdAt;
-    }
-    if (!this.object.canceledAt && this.isCanceled()) {
-      this.object.canceledAt = createdAt;
-    }
-  },
+  
 
   addDateCodes: function (group) {
     var generator = new DateCodeGenerator(group);
