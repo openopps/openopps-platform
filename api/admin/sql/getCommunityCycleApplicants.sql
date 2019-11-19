8315dbc108aac11aae4c0297b2fb37b7d53c7c18
tanichacko <40576337+tanichacko@users.noreply.github.com>
2019-11-19 14:36:02 -0500

    View applicant - sort by
    - Add sort by options and functionality
    - update query
    
    #4133

--- api/admin/sql/getCommunityCycleApplicants.sql
+++ api/admin/sql/getCommunityCycleApplicants.sql
@@ -1,6 +1,10 @@
+
+with applications as (
 SELECT 
 midas_user.id As "applicantId", midas_user.name as "applicantName", midas_user.username,app.cycle_id,
-app.application_id AS "id", app.submitted_at AS "submittedAt", 
+ coalesce(midas_user.given_name, (string_to_array(trim(midas_user.name), ' '))[array_lower(string_to_array(trim(midas_user.name), ' '), 1)]) as given_name,
+coalesce(midas_user.last_name, (string_to_array(trim(midas_user.name), ' '))[array_upper(string_to_array(trim(midas_user.name), ' '), 1)]) as last_name, 
+app.application_id AS "id", app.submitted_at AS "submittedAt", app.community_id,
   comm.community_name AS "communityName", c.name AS "cycleName", c.cycle_start_date AS "cycleStartDate",
   c.apply_end_date AS "applyEndDate", app.updated_at AS "updatedAt", phase."name", phase."sequence", 
   case when app.internship_completed_at is not null then true else false end as "internshipComplete",
@@ -28,4 +32,41 @@ app.application_id AS "id", app.submitted_at AS "submittedAt",
   INNER JOIN cycle c ON app.cycle_id = c.cycle_id 
   join midas_user on midas_user.id = app.user_id
   LEFT JOIN phase ON c.phase_id = phase.phase_id 
-  WHERE app.community_id = ? and app.cycle_id= ?
+)
+
+select applications.*, 
+(	
+		case
+		when applications."submittedAt" is not null and applications."sequence"=3 and applications."taskState"='completed'
+		and applications."internshipComplete"='true' 
+		then 'Completed'
+		when applications."submittedAt" is not null and applications."sequence"=3 and applications."taskState"='completed'
+		 and applications."internshipComplete"='false' and applications."reviewProgress"='Primary'
+		then 'Not completed'
+		when applications."submittedAt" is not null and applications."sequence"=3 and applications."taskState"='completed'
+		  and applications."reviewProgress"='Alternate'
+		then 'Alternate'
+		when applications."submittedAt" is not null and applications."sequence"=3 and applications."taskState"!='completed'
+		 and  applications."reviewProgress"='Primary'
+		then 'Primary Select'
+		when applications."submittedAt" is not null and applications."sequence"=3 and applications."taskState"!='completed'
+		 and  applications."reviewProgress"='Alternate'
+		 then 'Alternate Select'		
+		when applications."submittedAt" is null 
+		and applications."applyEndDate">current_timestamp  
+		then 'In progress'	
+	   when applications."submittedAt" is null 
+		and applications."applyEndDate"< current_timestamp  
+		then 'Not submitted'
+		 when applications."submittedAt" is not  null and applications."sequence"!=3
+		then 'Applied'
+		else 'Not selected'
+		end
+		 ) as "status"
+
+from applications
+ where applications.community_id = ? and applications.cycle_id= ?
+order by [order by]
+
+
+
