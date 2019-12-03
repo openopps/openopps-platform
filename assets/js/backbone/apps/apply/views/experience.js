60758c2253939bf38afaaaa86fbb43bd410983ab
tanichacko <40576337+tanichacko@users.noreply.github.com>
2019-12-03 13:59:02 -0500

    Fix charecter count update when edit
    #4125

--- assets/js/backbone/apps/apply/views/experience.js
+++ assets/js/backbone/apps/apply/views/experience.js
@@ -309,7 +309,7 @@ var experience = {
     var template = templates.applyAddExperience(data);
     this.$el.html(template);
     this.$el.localize();
-    this.renderProcessFlowTemplate({ currentStep: Math.max(this.data.currentStep, 2), selectedStep: 2 });
+    this.renderProcessFlowTemplate({ currentStep: Math.max(this.data.currentStep, 2), selectedStep: 2 }); 
     this.initializeCountriesSelect();
     window.scrollTo(0, 0);
   },
@@ -328,6 +328,7 @@ var experience = {
     this.$el.html(template);
     this.$el.localize();
     this.renderProcessFlowTemplate({ currentStep: Math.max(this.data.currentStep, 2), selectedStep: 2 });
+    experience.characterCount.bind(this)(); 
     this.initializeCountriesSelect();
     $('#apply_country').select2('data', { 
       id: data.country.countryId, 
