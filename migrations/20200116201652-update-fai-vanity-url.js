f24c3cf16df84331b2ab66b5c1ceec8ab820a427
Brice Corum <brice.corum@opm.gov>
2020-01-16 15:20:02 -0500

    Update FAI vanity URL
    - Change vanity URL for FAI community
    
    #4381

---
+++ migrations/20200116201652-update-fai-vanity-url.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return null;
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return Promise.all([
+    db.runSql(`update community set vanity_url = 'acquisition' where community_name = 'Federal Acquisition Professionals'`),
+  ]);
+};
+
+exports.down = function (db) {
+  return Promise.all([
+    db.runSql(`update community set vanity_url = 'fai' where community_name = 'Federal Acquisition Professionals'`),
+  ]);
+};
+
+exports._meta = {
+  'version': 1,
+};
