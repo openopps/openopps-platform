ca062df40a0b7e0ae05f0f4b8f300a9185ef2414
Alex Prokop <alex.prokop@gmail.com>
2020-05-14 16:33:24 -0400

    adds departments

---
+++ migrations/20200514193400-remove-fed-add-ca-departments.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var _ = require('lodash');
+var yaml = require('js-yaml');
+var fs = require('fs');
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.runSql(`
+    delete from tagentity where type = 'agency' 
+  `, callback);
+
+  var agencyFile = 'tools/init-tag-data/agency.yml';
+  if (fs.existsSync(agencyFile)) {
+    var agencyFileObjects = yaml.safeLoad(fs.readFileSync(agencyFile).toString());
+    agencyFileObjects.forEach(agency => {
+	  db.insert('agency', [
+	    'name',
+	    'abbr',
+	    'domain',
+	    'slug',
+	    'allow_restrict_agency',
+	    'created_at',
+	    'code',
+	    'parent_code',
+	    'is_disabled'
+	  ], [
+	    agency.title,
+	    agency.acronym,
+	    '',
+	    agency.acronym.toLowerCase(),
+	    true,
+	    new Date(),
+	    agency.acronym,
+	    'CA',
+	    false
+	  ], callback);
+    });
+  } else {
+  	throw "No agency file";
+  }
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
