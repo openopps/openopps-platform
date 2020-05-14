bebe8d9fa886d3f782a5a25d374496aace9bae80
bmcorum <bmcorum@gmail.com>
2020-05-14 14:16:12 -0400

    Update package
    - Update package version
    - Add a database update helper function
    
    #44530

--- db/client.js
+++ db/client.js
@@ -1,6 +1,7 @@
 const setup = require('../app/openopps-setup');
 const { Pool } = require('pg');
 const pool = new Pool(setup.dbConfig);
+const { update } = require('./helpers');
 
 const client = {};
 
@@ -10,6 +11,25 @@ client.query = async function (text, params) {
   const duration = Date.now() - start;
   console.log('executed query', { text, duration, rows: result.rowCount });
   return result;
+};
+
+client.update = async (entity, conditions, fields) => {
+  if (!entity)
+    throw new Error('no entity table specified');
+  if (_.isEmpty(conditions))
+    throw new Error('no conditions specified');
+
+  const { text, values } = update(entity, conditions, fields);
+
+  try {
+    var result = await pool.query(text, values);
+    const duration = Date.now() - start;
+    console.log('executed query', { text, duration, rows: result.rowCount });
+    return result;
+  } catch (err) {
+    log.error(err);
+    throw err;
+  }
 };
 
 module.exports = client;
